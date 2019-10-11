d6d873865d7c10609c5d8db4679d51979c4e24e8
Cole Mundus <mundus2018@gmail.com>
2019-10-11 01:16:03 -0500

    fix data collector

--- data_collector.py
+++ data_collector.py
@@ -1,5 +1,5 @@
-import Tkinter as tk
-from PIL import Image, ImageTk
+import tkinter as tk
+from PIL import Image
 import cv2
 
 print('Starting data collector')
@@ -12,12 +12,13 @@ ret = True
 while ret:
 	ret, frame = camera.read()
 	cv2.imshow("Data Capture", frame)
+	k = cv2.waitKey(1)
 	if k%256 == 27:
 		break
 	elif k%256 == 32:
-		img = f"{count.zfill(5)}.png"
+		img = f"training_data/capture/{str(count).zfill(5)}.png"
 		cv2.imwrite(img, frame)
-		print(f'Wrote to file {count.zfill(5)}.png')
-		img += 1
+		print(f'Wrote to file '+img)
+		count += 1
 camera.release()
 cv2.destroyAllWindows()
