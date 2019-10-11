9306d2f6df4fe4a75133e600a06c86cbb40d4f23
Cole Mundus <mundus2018@gmail.com>
2019-10-11 12:08:45 -0500

    add data collector modes

--- data_collector.py
+++ data_collector.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from PIL import Image
+from random import choice
 import cv2
+import os
 
 alpha = 'abcdefghijklmnopqrstuvwxyz'
 
@@ -9,20 +11,46 @@ print('Starting data collector')
 camera = cv2.VideoCapture(0)
 cv2.namedWindow("Data Capture")
 
+print('Please select capture mode: [random](1) [selected](2)')
+while True:
+	mode = input('Capture mode [1] 2: ')
+	if mode in ['1', '2', 'random', 'selected']:
+		break
+	else:
+		print('Invalid mode selected, please select between random or selected')
+
+print('Starting capture, press ESC to close')
 count = 0
 ret = True
+printed = False
 while ret:
 	ret, frame = camera.read()
 	cv2.imshow("Data Capture", frame)
 	k = cv2.waitKey(1)
 	if k%256 == 27:
 		break
-	elif k%256 == 32:
-		img = f"training_data/capture/{str(count).zfill(5)}.png"
-		cv2.imwrite(img, frame)
-		print(f'Wrote to file '+img)
-		count += 1
-	elif 123 > k%256 > 96:
-		print(alpha[k%256-97])
+	if mode in ['random', '1']:
+		if not printed:
+			key = choice(alpha).capitalize()
+			print(key)
+		printed = True
+		if k%256 == 32:
+			if not os.path.exists(f'training_data/capture/{key}/'):
+				os.makedirs(f'training_data/capture/{key}/')
+			img = f"training_data/capture/{key}/{str(count).zfill(5)}.png"
+			cv2.imwrite(img, frame)
+			print(f'Wrote to file '+img)
+			printed = False
+			count += 1
+	elif mode in ['selected', '2']:
+		if 123 > k%256 > 96:
+			key = alpha[k%256-97].capitalize()
+			if not os.path.exists(f'training_data/capture/{key}/'):
+				os.makedirs(f'training_data/capture/{key}/')
+			img = f"training_data/capture/{key}/{str(count).zfill(5)}.png"
+			cv2.imwrite(img, frame)
+			print(f'Wrote to file '+img)
+			count += 1
+
 camera.release()
 cv2.destroyAllWindows()
