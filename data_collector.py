358f13eb0d7328ccb265412d9cb478f60faee4d1
Mundus <mundus@uiowa.edu>
2019-10-11 14:59:58 -0500

    image formating

--- data_collector.py
+++ data_collector.py
@@ -13,7 +13,7 @@ cv2.namedWindow("Data Capture")
 
 print('Please select capture mode: [random](1) [selected](2)')
 while True:
-	mode = input('Capture mode [1] 2: ')
+	mode = '1'#input('Capture mode [1] 2: ')
 	if mode in ['1', '2', 'random', 'selected']:
 		break
 	else:
@@ -25,7 +25,13 @@ ret = True
 printed = False
 while ret:
 	ret, frame = camera.read()
-	cv2.imshow("Data Capture", frame)
+	frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	edDet = cv2.Canny(frame,100,120)
+	#mask = cv2.createBackgroundSubtractorKNN().apply(frame)
+	with_rect = cv2.rectangle(frame,(60,60),(375,375),(0,255,0),2)
+	cv2.imshow("Data Capture", with_rect)
+	cv2.imshow("Edge Detection", edDet)
+	cv2.imshow("Cropped", frame[60:375, 60:375])
 	k = cv2.waitKey(1)
 	if k%256 == 27:
 		break
