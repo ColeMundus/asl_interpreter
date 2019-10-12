166d5f1ddf64b2979fdf69272a42ac9aeca5ca6c
Cole Mundus <mundus2018@gmail.com>
2019-10-11 19:12:02 -0500

    refactor data collector

--- data_collector.py
+++ data_collector.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from PIL import Image
 from random import choice
+import glob
 import cv2
 import os
 
@@ -20,21 +21,41 @@ while True:
 		print('Invalid mode selected, please select between random or selected')
 
 print('Starting capture, press ESC to close')
-count = 0
 ret = True
 printed = False
+
+def write_original_image(cv2, frame, letter):
+	if not os.path.exists(f'training_data/capture/original_images/'):
+		os.makedirs(f'training_data/capture/original_images/')
+	try:
+		last_number = int(glob.glob(f'training_data/capture/original_images/{letter}_*.png')[-1][-8:-4])
+	except Exception as e:
+		last_number = 0
+	path = f'training_data/capture/original_images/{letter}_{str(last_number+1).zfill(4)}.png'
+	cv2.imwrite(path, frame[60:375, 60:375])
+	print('Wrote to file ' + path)
+	return path
+
+def write_data(frame, letter):
+	data_string = ''.join([str(int(i)) for j in frame/255 for i in j])
+	with open('training_data/capture/output.csv', 'a') as f:
+		f.write(f'{alpha.index(letter.lower())},{data_string}\n')
+
 while ret:
 	ret, frame = camera.read()
+
+	raw_image = cv2.rectangle(frame,(60,60),(375,375),(0,255,0),2)
+	cv2.imshow("Data Capture", raw_image)
+
 	frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	edDet = cv2.Canny(frame,100,120)
-	#mask = cv2.createBackgroundSubtractorKNN().apply(frame)
-	with_rect = cv2.rectangle(frame,(60,60),(375,375),(0,255,0),2)
 	downscale = cv2.resize(frame[60:375, 60:375], (128, 128), interpolation=cv2.INTER_LINEAR)
-	downscale = cv2.Canny(downscale,100,120)
 	upscale = cv2.resize(downscale, (512, 512), interpolation=cv2.INTER_NEAREST)
-	cv2.imshow("Data Capture", with_rect)
-	cv2.imshow("Edge Detection", edDet)
-	cv2.imshow("Cropped", upscale)
+	cv2.imshow("Down Scaled Capture", upscale)
+
+	data_output = cv2.Canny(downscale,100,120)
+	data_output = cv2.resize(data_output, (512, 512), interpolation=cv2.INTER_NEAREST)
+	cv2.imshow("Cropped", data_output)
+
 	k = cv2.waitKey(1)
 	if k%256 == 27:
 		break
@@ -44,22 +65,14 @@ while ret:
 			print(key)
 		printed = True
 		if k%256 == 32:
-			if not os.path.exists(f'training_data/capture/{key}/'):
-				os.makedirs(f'training_data/capture/{key}/')
-			img = f"training_data/capture/{key}/{str(count).zfill(5)}.png"
-			cv2.imwrite(img, frame)
-			print(f'Wrote to file '+img)
+			write_original_image(cv2, frame, key)
+			write_data(data_output, key)
 			printed = False
-			count += 1
 	elif mode in ['selected', '2']:
 		if 123 > k%256 > 96:
 			key = alpha[k%256-97].capitalize()
-			if not os.path.exists(f'training_data/capture/{key}/'):
-				os.makedirs(f'training_data/capture/{key}/')
-			img = f"training_data/capture/{key}/{str(count).zfill(5)}.png"
-			cv2.imwrite(img, frame)
-			print(f'Wrote to file '+img)
-			count += 1
+			write_original_image(cv2, frame, key)
+			write_data(data_output, key)
 
 camera.release()
 cv2.destroyAllWindows()
